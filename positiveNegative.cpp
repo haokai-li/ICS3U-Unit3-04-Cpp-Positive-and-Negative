776a2981ecdbbe4496147020c54bf1a531844cb6
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-09-21 16:19:24 +0000

    positive and negative

---
+++ positiveNegative.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) 2021 haokai All rights reserved
+//
+// Created by: Haokai
+// Created on: Sept 2021
+// This Program check that the integer is positive, negative and zero
+#include <iostream>
+#include <random>
+
+int main() {
+  // This Program check that the integer is positive, negative and zero
+
+  int integer;
+
+  // input
+  std::cout << "Enter an integer: ";
+  std::cin >> integer;
+  std::cout << "" << std::endl;
+
+  // process
+  if (integer > 0) {
+    // output
+    std::cout << integer << " is a positive number." << std::endl;
+  } else if (integer < 0) {
+    // output
+    std::cout << integer << " is a negative number." << std::endl;
+  } else {
+    std::cout << integer << " is just zero." << std::endl;
+  }
+
+  std::cout << "\nDone" << std::endl;
+}
